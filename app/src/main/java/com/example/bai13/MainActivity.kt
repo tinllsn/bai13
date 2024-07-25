2ed4a70080c49da5bf9f23a79ef4d0ab1c5029eb
tinllsn <tinllsn1674@gmail.com>
2024-07-25 23:33:40 +0700

    bai13

---
+++ app/src/main/java/com/example/bai13/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.bai13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bai13.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
